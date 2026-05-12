c7470cfb7eb7fbf2c8c4e3885f4de6a5b3fbc25c
Pranay Verma <pranayverma821@gmail.com>
2026-05-12 23:29:28 +0530

    fix: update backend port and start script for render deployment

--- Backend1/server.js
+++ Backend1/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 
 connectDB().then(() => {
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   socket.setupSocket(server)
   server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
